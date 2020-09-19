448ffc24ea0abb7e6f9a53c88b5423514c262cbe
dylanleong11 <33636014+dylanleong11@users.noreply.github.com>
2020-09-19 12:22:01 -0700

    Add files via upload

---
+++ OneMotorExample.ino
@@ -0,0 +1,67 @@
+//L293D
+//enable 
+const int enableA = 7;
+const int enableB = 2;
+
+//Motor A
+const int motorPin1  = 6;  // Pin 14 of L293
+const int motorPin2  = 5;  // Pin 10 of L293
+//Motor B
+const int motorPin3  = 4; // Pin  7 of L293
+const int motorPin4  = 3;  // Pin  2 of L293
+
+//This will run only one time.
+void setup(){
+ 
+    //Set pins as outputs
+    pinMode(enableA, OUTPUT);
+    pinMode(enableB, OUTPUT);
+    pinMode(motorPin1, OUTPUT);
+    pinMode(motorPin2, OUTPUT);
+    pinMode(motorPin3, OUTPUT);
+    pinMode(motorPin4, OUTPUT);
+    
+    //enable
+    analogWrite(enableA, 200);
+    analogWrite(enableB, 200);
+  
+    //Motor Control - Motor A: motorPin1,motorpin2 & Motor B: motorpin3,motorpin4
+
+    //This code  will turn Motor A clockwise for 2 sec.
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(2000); 
+    //This code will turn Motor A counter-clockwise for 2 sec.
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    delay(2000);
+    
+    //This code will turn Motor B clockwise for 2 sec.
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    delay(2000); 
+    //This code will turn Motor B counter-clockwise for 2 sec.
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    delay(2000);    
+    
+    //And this code will stop motors
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+  
+}
+
+void loop(){
+  
+
+}
