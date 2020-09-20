3fecc72f5a9dcddcb2d8943bc0b5fb28af339634
kellywzheng <54915382+kellywzheng@users.noreply.github.com>
2020-09-20 00:24:36 -0700

    Update main.ino

--- arduino/main.ino
+++ arduino/main.ino
@@ -3,44 +3,35 @@
 //Sensors
 #define trigPinFrontLeft 49
 #define echoPinFrontLeft 48
-#define trigPinFrontRight 2
-#define echoPinFrontRight 3
-#define trigPinLeft 6
-#define echoPinLeft 7
-#define trigPinRight 10
-#define echoPinRight 11
+#define trigPinFrontRight 47
+#define echoPinFrontRight 46
+#define trigPinLeft 45
+#define echoPinLeft 44
+#define trigPinRight 43
+#define echoPinRight 42
 
 // L298N control pins
-//CONTROL MOTOR 1
-#define enableA 7
-#define enableB 2
+// Enable front 
+#define F_ENA 9
+#define F_ENB 8
+//FRONT LEFT WHEEL
+#define F_IN1 7 //6   // Pin 14 of L293
+#define F_IN2 5   // Pin 10 of L293
+//FRONT RIGHT WHEEL
+#define F_IN3 6 //4   // Pin  7 of L293
+#define F_IN4 3   // Pin  2 of L293
 
+//BACK MOTOR
+#define B_ENA 11
+#define B_ENB 12
 //Motor A
-#define motorPin1 6   // Pin 14 of L293
-#define motorPin2 5   // Pin 10 of L293
+#define B_IN1 23  // Pin 14 of L293
+#define B_IN2 22  // Pin 10 of L293
 //Motor B
-#define motorPin3 4   // Pin  7 of L293
-#define motorPin4 3   // Pin  2 of L293
+#define B_IN3 27   // Pin  7 of L293
+#define B_IN4 26   // Pin  2 of L293
 
-//CONTROL MOTOR 2
-#define enable2A 24
-#define enable2B 25
-
-//Motor A
-#define motor2Pin1 23  // Pin 14 of L293
-#define motor2Pin2 22  // Pin 10 of L293
-//Motor B
-#define motor2Pin3 27   // Pin  7 of L293
-#define motor2Pin4 26   // Pin  2 of L293
-
-// LED
-#define ledPin 13
-//SERVO
-#define servoPin 9
-// BAUD
 #define baud 9600
-
-//Servo myServo;
 
 void setup() {
     /*
@@ -48,7 +39,7 @@ void setup() {
     */
     pinMode(trigPinFrontLeft, OUTPUT);
     pinMode(echoPinFrontLeft, INPUT);
-    /*
+
     pinMode(trigPinFrontRight, OUTPUT);
     pinMode(echoPinFrontRight, INPUT);
     
@@ -57,44 +48,29 @@ void setup() {
     
     pinMode(trigPinRight, OUTPUT);
     pinMode(echoPinRight, INPUT);
-    */
-    
-    /* 
-    LED
-    */
-    //pinMode(ledPin, OUTPUT);
-
-    // Servo
-    //myServo.attach(servoPin);
 
     /* 
     Motors
     */
-    pinMode(enableA, OUTPUT);
-    pinMode(enableB, OUTPUT);
-    pinMode(motorPin1, OUTPUT);
-    pinMode(motorPin2, OUTPUT);
-    pinMode(motorPin3, OUTPUT);
-    pinMode(motorPin4, OUTPUT);
-
-    pinMode(enable2A, OUTPUT);
-    pinMode(enable2B, OUTPUT);
-    pinMode(motor2Pin1, OUTPUT);
-    pinMode(motor2Pin2, OUTPUT);
-    pinMode(motor2Pin3, OUTPUT);
-    pinMode(motor2Pin4, OUTPUT);
+    pinMode(F_ENA, OUTPUT);
+    pinMode(F_ENB, OUTPUT);
+    pinMode(F_IN1, OUTPUT);
+    pinMode(F_IN2, OUTPUT);
+    pinMode(F_IN3, OUTPUT);
+    pinMode(F_IN4, OUTPUT);
+    pinMode(B_ENA, OUTPUT);
+    pinMode(B_ENB, OUTPUT);
+    pinMode(B_IN1, OUTPUT);
+    pinMode(B_IN2, OUTPUT);
+    pinMode(B_IN3, OUTPUT);
+    pinMode(B_IN4, OUTPUT);
     
-    //enable
-    analogWrite(enableA, 200);
-    analogWrite(enableB, 200);
-
-    analogWrite(enable2A, 200);
-    analogWrite(enable2B, 200);
-
     // Serial communication
     Serial.begin(baud);
 }
 
 void loop() {
     readSensors();
+    demoOne();
+    //demoTwo();
 }
